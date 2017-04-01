9c915172d50b4b89e471ce422881432612dd10e5
occultsoul <rachit.prfnl@gmail.com>
2017-04-01 22:28:19 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,26 @@
+/*Problem statement
+Given two numbers, add them and print their sum
+
+Input
+Input contains two numbers between 1 and 100, separated by a space
+
+Output
+Print the sum of the 2 numbers
+*/
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+// Include headers as needed
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cin>>num1>>num2;
+    
+    int sum= (num1+num2);
+    cout<<sum<<"\n";
+    return 0;
+}
